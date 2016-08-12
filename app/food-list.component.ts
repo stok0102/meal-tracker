b60dd718997b755a925bd70ec850edd57c645db2
Caleb Stokka <caleb.stokka@gmail.com>
2016-08-12 09:56:23 -0700

    add food list component

---
+++ app/food-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { Food } from './food.model';
+import { FoodComponent } from './food.component';
+
+@Component({
+  selector: 'food-list',
+  inputs: ['foodList'],
+  outputs: ['onFoodSelect'],
+  directives: [FoodComponent],
+  template: `
+    <div *ngFor="#food of foodList ">
+      <food [currentFood]="food" (click)="foodClicked(food)"></food>
+    </div>
+  `
+})
+
+export class FoodListComponent {
+  public foodList: Food[];
+  public selectedFood: Food;
+  public onFoodSelect: EventEmitter<Food>;
+  constructor() {
+    this.onFoodSelect = new EventEmitter();
+  }
+  foodClicked(clickedFood: Food): void {
+    this.onFoodSelect.emit(clickedFood);
+  }
+}
